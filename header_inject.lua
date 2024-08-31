4eea00acc777edcdd4e297c6a6272cff061082cb
Gonen Gazit <gonengazit@gmail.com>
2024-08-31 22:41:49 +0300

    inject pico-8 header into the cart
    The pico-8 inject defines most builtins as as locals, as well as
    defining the glyph constants

---
+++ header_inject.lua
@@ -0,0 +1,33 @@
+█=█ or 0.5
+▒=▒ or 23130.5
+🐱=🐱 or 20767.5
+⬇️=⬇️ or 3
+░=░ or 32125.5
+✽=✽ or -18402.5
+●=● or -1632.5
+♥=♥ or 20927.5
+☉=☉ or -19008.5
+웃=웃 or -26208.5
+⌂=⌂ or -20192.5
+⬅️=⬅️ or 0
+😐=😐 or -24351.5
+♪=♪ or -25792.5
+🅾️=🅾️ or 4
+◆=◆ or -20032.5
+…=… or -2560.5
+➡️=➡️ or 1
+★=★ or -20128.5
+⧗=⧗ or 6943.5
+⬆️=⬆️ or 2
+ˇ=ˇ or -2624.5
+∧=∧ or 31455.5
+❎=❎ or 5
+▤=▤ or 3855.5
+▥=▥ or 21845.5
+;
+
+local time,sub,chr,ord,tostr,tonum,add,del,deli,clip,color,pal,palt,fillp,pget,pset,sget,sset,fget,fset,circ,circfill,rect,rectfill,oval,ovalfill,line,spr,sspr=
+      time,sub,chr,ord,tostr,tonum,add,del,deli,clip,color,pal,palt,fillp,pget,pset,sget,sset,fget,fset,circ,circfill,rect,rectfill,oval,ovalfill,line,spr,sspr
+local mget,mset,tline,peek,poke,peek2,poke2,peek4,poke4,memcpy,memset,max,min,mid,flr,ceil,cos,sin,atan2,rand,srand,band,bor,bxor,bnot,shl,shr,lshr,rotl,rotr=
+      mget,mset,tline,peek,poke,peek2,poke2,peek4,poke4,memcpy,memset,max,min,mid,flr,ceil,cos,sin,atan2,rand,srand,band,bor,bxor,bnot,shl,shr,lshr,rotl,rotr;
+
